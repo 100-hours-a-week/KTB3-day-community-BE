9c9dcb410c067f1c865e6dc345f24542c4e69548
Day HONG <tot032874@gmail.com>
2025-10-23 11:24:28 +0900

    feat/login : using session

---
+++ src/main/java/com/demo/community/auth/dto/AuthResponseDTO.java
@@ -0,0 +1,23 @@
+package com.demo.community.auth.dto;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+public class AuthResponseDTO {
+
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Getter
+    @Setter
+    public static class LoginResponse{
+        @NotNull
+        private Long userId;
+        @NotNull
+        private String email;
+        @NotNull
+        private String nickname;
+        @NotNull
+        private String profileImage;
+    }
+}
